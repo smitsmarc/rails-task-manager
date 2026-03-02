bed7727b2929e2e2e3509e81053814f859f0c5ca
marc smits <mrcsmits@gmail.com>
2026-03-02 18:10:51 +1100

    Tasks List

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: "new_task"
+  get "tasks/:id", to: "tasks#show", as: "task"
+  post "tasks", to: "tasks#create"
+  delete "tasks/:id", to: "tasks#destroy", as: "destroy_task"
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  patch "tasks/:id", to: "tasks#update"
 end
